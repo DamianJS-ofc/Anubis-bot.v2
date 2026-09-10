a5dfa5d501bd67d93389d4203ed6834929257c7f
DamianJS-ofc <damiandeveloper122@gmail.com>
2026-09-10 00:41:01 -0300

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,24 @@
+global.botname = 'Anubis V2'
+global.owner = [["5492645746772", "DamianJS-ofc", true]]
+global.prefixes = ['.', '!', '#']
+global.banner = 'https://raw.githubusercontent.com/JTxs00/uploads/main/1788590384027.jpeg'
+global.channelName = '🎋 Anubis bot V2'
+global.channelId = '120363425415754278@newsletter'
+global.channelLink = 'https://whatsapp.com/channel/0029Vb7vqNDCsU9MnOn8UN0U'
+global.botType = 'Principal'
+global.web = 'https://anubissuport.netlify.app'
+
+export default {
+  botName: global.botname,
+  botType: global.botType,
+  owner: global.owner,
+  prefixes: global.prefixes,
+  prefix: '.',
+  banner: global.banner,
+  bannerUrl: global.banner,
+  channelName: global.channelName,
+  channelId: global.channelId,
+  channelLink: global.channelLink,
+  channel: global.channelLink,
+  web: global.web
+}
